1209f326988e43b4e708a3431f98353edde1a85f
Cicatrice-Vlad <vladbargaoanu@yahoo.com>
2023-01-24 23:24:57 +0000

    added some more code in regards to the current hour and logged it in as well as comments below and above each code line where applicable to explain the meaning

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ console.log(currHour);
 // created current hour method to be found in real life time. Also, logged it in to the console.
 currHour = 12;
 // establishing the current hour randomly to appeal more to the exercise example.
+function generateDayPlanner(starthour, endhour) {
+  // created a method within a function to establish it later when we call and append all elements as the start hour and end hour of the planner.
+  // using the for loop below to create all the possible times and variables that would be followed to be appended.
+  for (i = starthour; i < endhour; i++) {
+    var classStyle = "";
+    if (i < currHour) {
+    }
+  }
+}
